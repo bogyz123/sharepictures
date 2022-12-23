d29a1f161845b821e91c07a456627d2e00fa5c07
bogyz123 <114313361+bogyz123@users.noreply.github.com>
2022-12-23 22:15:36 +0100

    Add files via upload

---
+++ src/fire_connection.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+import { getStorage } from "firebase/storage";
+const firebaseConfig = {
+  apiKey: "AIzaSyCGC2gTbdbw3qNG1bicebI8YPXs1tvmWYM",
+  authDomain: "uploadimages-43ff9.firebaseapp.com",
+  projectId: "uploadimages-43ff9",
+  storageBucket: "uploadimages-43ff9.appspot.com",
+  messagingSenderId: "842159621062",
+  appId: "1:842159621062:web:8497201afa14ac4aa1e8c7"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
